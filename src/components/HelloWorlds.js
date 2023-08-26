8439151a645938c059b981f4afcde7ee258b589d
daylorp <139701274+daylorp@users.noreply.github.com>
2023-08-26 14:24:08 -0300

    Props

---
+++ src/components/HelloWorlds.js
@@ -0,0 +1,12 @@
+import Frase from './Frase'
+
+function HelloWorlds() {
+    return (
+        <div>
+            <h1>Componente</h1>
+            <Frase />
+        </div>
+    )
+}
+
+export default HelloWorlds
