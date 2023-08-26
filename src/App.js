f395ef20f58bde4601785586c53801f82a2c4662
daylorp <139701274+daylorp@users.noreply.github.com>
2023-08-26 14:43:57 -0300

    CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa'
+import Frase from './components/Frase'
 
 function App() {
 
@@ -9,6 +10,9 @@ const nome = "Maria";
     <div className="App">
       <SayMyName nome={nome}/>
       <Pessoa nome="Dayana" idade="25" profissao="Programadora" foto="https://via.placeholder.com/150"/>
+    
+      <Frase />
+
     </div>
   );
 }
--- src/components/Pessoa.js
+++ src/components/Pessoa.js
@@ -1,9 +1,9 @@
 function Pessoa({nome, idade, profissao, foto}) {
     return (
         <div>
+            <h1>Ficha Cadastral</h1>
             <img src={foto} alt={nome} />
-            <h2>Nome: {nome}</h2>
-            <p>Idade: {idade}</p>
+            <h2>{nome} | {idade} anos</h2>
             <p>Profissão: {profissao}</p>
         </div>
     )
