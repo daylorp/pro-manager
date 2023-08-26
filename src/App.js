10d7b78259ddf4d9490f58e7b095fbd9ec591963
daylorp <139701274+daylorp@users.noreply.github.com>
2023-08-26 14:54:02 -0300

    Fragments

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import './App.css'
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa'
 import Frase from './components/Frase'
+import List from './components/List'
 
 function App() {
 
 const nome = "Maria";
   return (
     <div className="App">
+
       <SayMyName nome={nome}/>
       <Pessoa nome="Dayana" idade="25" profissao="Programadora" foto="https://via.placeholder.com/150"/>
     
       <Frase />
 
+      <List />
+      
     </div>
   );
 }
